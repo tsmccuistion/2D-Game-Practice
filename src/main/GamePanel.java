9441ea882d23afe7c757ad77c5888150fa6b4081
Tyler McCuistion <tmccuist@colostate.edu>
2025-04-02 08:46:18 -0600

    initital code

---
+++ src/main/GamePanel.java
@@ -0,0 +1,78 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+
+public class GamePanel extends JPanel implements Runnable {
+    //SCREEN SETTINGS
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale; //48x48 tile
+    
+    //Game Dimensions
+    final int maxScreenColumn = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenColumn; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        player.draw(g2);
+        g2.dispose();
+    }
+}
